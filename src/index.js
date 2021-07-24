e695331bb6b527cdfc6826373881d843b26f3264
JinhuiKim <public.jinhui.kim@gmail.com>
2021-07-25 08:49:16 +0900

    #3.1.5. market 스토어 적용하기

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import App from "./App";
 // import registerServiceWorker from "./registerServiceWorker";
 import reportWebVitals from "./reportWebVitals";
 import CounterStore from "./stores/counter"; // 방금 만든 스토어 불러와줍니다.
+import MarketStore from "./stores/market";
 
 const counter = new CounterStore(); // 스토어 인스턴스를 만들고
+const market = new MarketStore();
 
 // 프로젝트에 스토어를 적용 할 때 Provider 라는 컴포넌트를 사용
 ReactDOM.render(
-  <Provider counter={counter}>
+  <Provider counter={counter} market={market}>
     {/* Provider 에 props 로 넣어줍니다. */}
     <App />
   </Provider>,
