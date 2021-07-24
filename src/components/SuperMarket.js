0146169d8a54b53d2e76e1b8ae47ea90612c596e
JinhuiKim <public.jinhui.kim@gmail.com>
2021-07-25 08:33:31 +0900

    #3.1.2. UI 구현하기 - 상품: ShopItem.js, ShopItem.css, ShopItemList.js - 장바구니: BasketItem.js, BasketItem.css, BasketItemList.js(하드코딩) - SuperMarket.js에 items, basket props 전달

--- src/components/SuperMarket.js
+++ src/components/SuperMarket.js
@@ -1,12 +1,15 @@
 import React from "react";
 import SuperMarketTemplate from "./SuperMarketTemplate";
+import ShopItemList from "./ShopItemList";
+import BasketItemList from "./BasketItemList";
 
 /**
- * SuperMarketTemplate 안에, 나중에 우리가 만들 Basket 과 ShopItemList 를 전달해서 보여줄 것
- * 지금은 아직 안만들었으니 nul
+ * SuperMarketTemplate 안에, Basket 과 ShopItemList 를 전달해서 보여줄 것
  */
 const SuperMarket = () => {
-  return <SuperMarketTemplate items={null} basket={null} />;
+  return (
+    <SuperMarketTemplate items={<ShopItemList />} basket={<BasketItemList />} />
+  );
 };
 
 export default SuperMarket;
