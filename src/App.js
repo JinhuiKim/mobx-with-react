a05b22036640e6d3401de7481d9293059ae8a28e
JinhuiKim <public.jinhui.kim@gmail.com>
2021-07-25 08:36:44 +0900

    #3.1.3. mobx-react-devtools 개발도구 설치 - yarn add mobx-react-devtools + App.js에 적용

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import Counter from "./components/Counter";
 import SuperMarket from "./components/SuperMarket";
 
+// 우리가 어떤 값을 바꿨을 때 어떠한 컴포넌트들이 영향을 받고, 업데이트는 얼마나 걸리고, 어떠한 변화가 일어났는지에 대한 세부적인 정보를 볼 수 있게 해줍니다.
+import DevTools from "mobx-react-devtools";
+
 class App extends Component {
   render() {
     return (
@@ -9,6 +12,7 @@ class App extends Component {
         <Counter />
         <hr />
         <SuperMarket />
+        {process.env.NODE_ENV === "development" && <DevTools />}
       </div>
     );
   }
