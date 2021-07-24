3ede4ee71345fa50ada4e67241a4327e692dae98
JinhuiKim <public.jinhui.kim@gmail.com>
2021-07-25 08:55:53 +0900

    #3.1.6. 기능 구현하기 - 아이템 추가

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./ShopItem.css";
 
-const ShopItem = ({ name, price }) => {
+const ShopItem = ({ name, price, onPut }) => {
   return (
-    <div className="ShopItem">
+    // ShopItem에서 클릭시 onPut에 현재 자신의 name과 price를 넣어서 호출하도록 설정
+    <div className="ShopItem" onClick={() => onPut(name, price)}>
       <h4>{name}</h4>
       <div>{price}원</div>
     </div>
--- src/components/ShopItemList.js
+++ src/components/ShopItemList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ShopItem from "./ShopItem";
+import { inject, observer } from "mobx-react"; // 불러오기
 
 const items = [
   {
@@ -20,9 +21,16 @@ const items = [
   },
 ];
 
-const ShopItemList = () => {
-  const itemList = items.map((item) => <ShopItem {...item} key={item.name} />);
+// **** onPut 함수 추가됨
+const ShopItemList = ({ onPut }) => {
+  const itemList = items.map((item) => (
+    <ShopItem {...item} key={item.name} onPut={onPut} />
+  ));
   return <div>{itemList}</div>;
 };
 
-export default ShopItemList;
+// **** inject, observer 적용
+// 함수형 컴포넌트에 inject 와 observer 를 적용할땐 이렇게 내보내주는 과정에서 사용하시면 조금 더 깔끔합니다.
+export default inject(({ market }) => ({
+  onPut: market.put,
+}))(observer(ShopItemList));
