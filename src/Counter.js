90b30ced24effc114130d7834ba9e8f341e8973f
JinhuiKim <public.jinhui.kim@gmail.com>
2021-07-24 19:59:56 +0900

    #2.1.3. Decorator 와 함께 사용하기 - Counter코드를 decorator로 작성 - How to remove experimentalDecorators warning in VSCode + settings.json: "js/ts.implicitProjectConfig.experimentalDecorators": true,

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
-import { decorate, observable, action } from "mobx";
+import { observable, action } from "mobx";
 import { observer } from "mobx-react";
 
+/**
+ * setState없이 값을 바꿔주면 알아서 렌더링 됨
+ * 코드 최하단의 observer가 observable 값이 변할 때 컴보넌트의 forceUpdate를 호출하게 함으로써 자동으로 변화가 화면에 반영됨
+ */
+// **** 최하단에 잇던 observer 가 이렇게 위로 올라옵니다.
+@observer
 class Counter extends Component {
-  number = 0;
+  @observable number = 0;
 
+  @action
   increase = () => {
     this.number++;
   };
 
+  @action
   decrease = () => {
     this.number--;
   };
@@ -24,14 +32,13 @@ class Counter extends Component {
   }
 }
 
-decorate(Counter, {
-  number: observable,
-  increase: action,
-  decrease: action,
-});
+// **** decorate 는 더 이상 필요 없어집니다.
+// decorate(Counter, {
+//   number: observable,
+//   increase: action,
+//   decrease: action
+// })
 
-export default observer(Counter);
-/**
- * setState없이 값을 바꿔주면 알아서 렌더링 됨
- * 코드 최하단의 observer가 observable 값이 변할 때 컴보넌트의 forceUpdate를 호출하게 함으로써 자동으로 변화가 화면에 반영됨
- */
+// export default observer(Counter);
+// **** observer 는 코드의 상단으로 올라갑니다.
+export default Counter;
