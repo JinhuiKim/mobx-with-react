4ccfcd6270b45957a7097148014853498c1fb49e
JinhuiKim <public.jinhui.kim@gmail.com>
2021-07-24 20:51:41 +0900

    #2.2.2. Provider 로 프로젝트에 스토어 적용 - registerServiceWorker -> reportWebVitals

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "mobx-react"; // MobX 에서 사용하는 Provider
+import "./index.css";
+import App from "./App";
+// import registerServiceWorker from "./registerServiceWorker";
+import reportWebVitals from "./reportWebVitals";
+import CounterStore from "./stores/counter"; // 방금 만든 스토어 불러와줍니다.
 
+const counter = new CounterStore(); // 스토어 인스턴스를 만들고
+
+// 프로젝트에 스토어를 적용 할 때 Provider 라는 컴포넌트를 사용
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider counter={counter}>
+    {/* Provider 에 props 로 넣어줍니다. */}
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
+// registerServiceWorker();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
