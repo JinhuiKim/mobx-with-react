de845e269b939bc2f3cce80e87e086c7ea0218da
JinhuiKim <public.jinhui.kim@gmail.com>
2021-07-25 11:07:56 +0900

    #3.1.7. 기능 구현하기 - 장바구니에 데이터 반영

--- src/components/BasketItem.js
+++ src/components/BasketItem.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./BasketItem.css";
+import { observer } from "mobx-react";
 
-const BasketItem = ({ name, price, count }) => {
+/**
+ * observer 를 하단이 아닌 위에서 설정해줘도 동일하게 작동
+ */
+const BasketItem = ({ name, price, count, onTake }) => {
+  // const BasketItem = observer(({ name, price, count, onTake }) => {
   return (
-    <div className="BasketItem">
+    <div className="BasketItem" onClick={() => onTake(name)}>
       <div className="name">{name}</div>
       <div className="price">{price}원</div>
       <div className="count">{count}</div>
@@ -11,5 +16,7 @@ const BasketItem = ({ name, price, count }) => {
     </div>
   );
 };
+// });
 
-export default BasketItem;
+export default observer(BasketItem);
+// export default BasketItem;
